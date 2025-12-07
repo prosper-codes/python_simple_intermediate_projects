047ba19f0bdf784f099ab8d4ce8f70d0c9fa241d
Takunda P J Nhau <tpjn02@gmail.com>
2025-12-07 23:44:15 +0100

    decode and compress file

--- file_compression/compress.py
+++ file_compression/compress.py
@@ -3,5 +3,10 @@ import zlib,base64
 data = open('files\demo.txt','r').read() 
 data_types = bytes(data,'utf-8')
 compressed_data = base64.b64encode(zlib.compress(data_types))
+decoded_data = compressed_data.decode('utf-8')
+compressed_file = open('compressed.txt','w')
+compressed_file.write(decoded_data)
+
+
 
 
