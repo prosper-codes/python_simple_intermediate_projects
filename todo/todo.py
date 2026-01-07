5fde036576d97572af6205f12544e964eab11bb3
Takunda P J Nhau <tpjn02@gmail.com>
2026-01-07 15:21:18 +0100

    improve todo

--- todo/todo.py
+++ todo/todo.py
@@ -1,3 +1,35 @@
-todo=input("ENTER A TODO: ")
-todo_list=[todo]
-print(todo_list)
+
+todo_list=[]
+
+
+while True:
+    user_action=input("Type add, view, edit, complete, stop type stop to cancel: ").strip()
+
+    match user_action:
+
+        case 'add':
+            todo=input("Enter a todo: ")
+            todo_list.append(todo)
+
+        case 'view':
+
+            for index,task in enumerate(todo_list):
+                index=index+1
+                print(f"{index}-{task}")
+
+        case 'edit':
+
+            num = int(input("enter number to edit: "))-1
+            new_todo=input("enter new todo: ")
+            todo_list[num]=new_todo
+        case 'complete':
+            num = int(input("enter the number of the to do you completed"))
+            todo_list.pop(num-1)
+
+        case 'stop':
+            break
+
+
+        case whatever:
+            print("you entered an invalid option")
+
