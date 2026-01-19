ddd03c80768afd2231bd66a142ff480a3ea8d2f7
Takunda P J Nhau <tpjn02@gmail.com>
2026-01-19 16:17:43 +0100

    modify todo app

--- todo/todo.py
+++ todo/todo.py
@@ -1,30 +1,58 @@
-
-todo_list=[]
-
-
 while True:
-    user_action=input("Type add, view, edit, complete, stop type stop to cancel: ").strip()
+    user_action=input("Type add, view, edit, complete, stop to cancel: ").strip()
 
     match user_action:
 
         case 'add':
-            todo=input("Enter a todo: ")
+            todo=input("Enter a todo: ")+"\n"
+
+            with open('todos_list.txt','r') as file:
+                todo_list=file.readlines()
+
             todo_list.append(todo)
 
+            with open('todos_list.txt','w') as file:
+                file.writelines(todo_list)
+
+
         case 'view':
+            
+            with open("todos_list.txt", 'r') as file:
+                todo_list = file.readlines()
 
             for index,task in enumerate(todo_list):
-                index=index+1
-                print(f"{index}-{task}")
+                 task = task.strip('\n')
+                 index=index+1
+                 print(f"{index}-{task}")
 
         case 'edit':
+             num = int(input("enter number to edit: "))-1
 
-            num = int(input("enter number to edit: "))-1
-            new_todo=input("enter new todo: ")
-            todo_list[num]=new_todo
+             with open("todos_list.txt", 'r') as file:
+                 todo_list = file.readlines()
+
+             new_todo =input("Enter your new todo: ")
+             todo_list[num]=new_todo + '\n'
+
+             with open("todos_list.txt", 'w') as file:
+                 file.writelines(todo_list)
+
+
+
+
         case 'complete':
+
             num = int(input("enter the number of the to do you completed"))
+
+            with open("todos_list.txt",'r') as file:
+                todo_list = file.readlines()
+            todo_completed = todo_list[num-1].strip('\n')
             todo_list.pop(num-1)
+
+            with open("todos_list.txt", 'w') as file:
+                file.writelines(todo_list)
+
+            print( f"Todo {todo_completed} was successfully completed and removed from the list")
 
         case 'stop':
             break
