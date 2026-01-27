d9e32367d9b35cf38402586232d94864fcb30c6c
Takunda P J Nhau <tpjn02@gmail.com>
2026-01-27 15:58:17 +0100

    add pdf template

---
+++ pdf_template/generate.py
@@ -0,0 +1,25 @@
+
+from fpdf import  FPDF
+import pandas as pd
+
+pdf = FPDF(orientation="P", unit="mm", format="A4")
+
+df = pd.read_csv("topics.csv")
+
+for index , row in df.iterrows():
+    pdf.add_page()
+
+    pdf.set_font(family="Times",style="B",size=13)
+    pdf.cell(
+        w=0,
+        h=12,
+        text=row["Topic"],
+        align="L",
+        new_x="LMARGIN",
+        new_y="NEXT"
+    )
+    for i in range(row["Pages"]-1):
+        pdf.add_page()
+
+
+pdf.output("output.pdf")
