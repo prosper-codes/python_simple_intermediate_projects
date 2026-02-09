ac57e5105ef8b3b3c96c745ccf11dc470be478ae
Takunda P J Nhau <tpjn02@gmail.com>
2026-02-09 23:46:26 +0100

    initial commit

---
+++ hotel_booking/main.py
@@ -0,0 +1,56 @@
+import pandas
+from matplotlib.style.core import available
+
+df = pandas.read_csv("hotels.csv", dtype={"id": str})
+
+
+class Hotel:
+    pass
+
+    def __init__(self, hotel_id):
+        self.hotel_id = hotel_id
+        self.name = df.loc[df["id"] == self.hotel_id, "name" ].squeeze()
+
+    def book(self):
+        """Book a hotel by changing its availability to no """
+        df.loc[df["id"] == self.hotel_id, "available"] = "no"
+        df.to_csv("hotels.csv", index=False)
+
+
+    def available(self):
+        """ Check if the hotel is available"""
+        availability = df.loc[df["id"] == self.hotel_id, "available" ].squeeze()
+
+        if availability == "yes":
+            return True
+        else:
+            return False
+
+class ReservationTicket:
+    def __init__(self, customer_name, hotel_object):
+        self.customer_name = customer_name
+        self.hotel= hotel_object
+
+    def generate(self):
+        content = f"""
+        Thank you for your reservation!
+        Here are your booking data:
+        Name: {self.customer_name}
+        Hotel name: {self.hotel.name}
+        """
+
+        return content
+
+
+print(df)
+hotel_ID = input("enter the id of hotel")
+hotel = Hotel(hotel_ID)
+
+
+if hotel.available():
+    hotel.book()
+    name = input("Enter your name: ")
+    reservation_ticket = ReservationTicket(customer_name=name, hotel_object=1hotel)
+    print(reservation_ticket.generate())
+else:
+    print("hotel not found")
